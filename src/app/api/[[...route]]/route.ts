2d1dc808e2e948764bec2db1d97146cec3d0ec5e
Ravikant Chauhan <ravikantchauhan246@gmail.com>
2024-04-07 10:52:19 +0530

    Implement search functionality in Home component

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -12,17 +12,51 @@ type EnvConfig={
     UPSTASH_REDIS_REST_URL:string
 }
 
-app.get('/search',(c)=>{
-    const {UPSTASH_REDIS_REST_TOKEN,UPSTASH_REDIS_REST_URL}= env<EnvConfig>(c)
+app.get('/search',async(c)=>{
+    try {
+        const {UPSTASH_REDIS_REST_TOKEN,UPSTASH_REDIS_REST_URL}= env<EnvConfig>(c)
     const redis = new Redis({
         token:UPSTASH_REDIS_REST_TOKEN,
         url:UPSTASH_REDIS_REST_URL,
     })
-    const query= c.req.query("q")
+    const start= performance.now()
+    // ---------
+    const query= c.req.query("q")?.toUpperCase()
     if(!query){
         return c.json({message: "Invalid search query"},{status:400})
     }
-    return c.json({})
+    const res=[]
+    const rank = await redis.zrank("terms",query)
+    if(rank!==null && rank !==undefined){
+        const temp=await redis.zrange<string[]>("terms",rank,rank+100)
+        for(const el of temp){
+            if(!el.startsWith(query)){
+                break
+            }
+            if(el.endsWith("*")){
+                res.push(el.substring(0,el.length-1))
+            }
+        }
+    }
+    //-------------------
+    const end = performance.now()
+
+
+    return c.json({
+        result: res,
+        duration: end-start
+    })
+        
+    } catch (err) {
+
+        console.error(err)
+        return c.json(
+            {results:[],message: 'Something went wrong.'},
+            {
+                status:500,
+            }
+        )
+    }
 })
 
 export const GET= handle(app)
